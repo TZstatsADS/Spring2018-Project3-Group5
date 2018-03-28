51ac1250e931f381a8b321133c7497f2709816fa
Sitong Chen <sc4283@columbia.edu>
2018-03-27 23:47:06 -0400

    update

--- doc/HSV_Feature.R
+++
@@ -1,9 +0,0 @@
-## HSV Feature
-
-source("http://bioconductor.org/biocLite.R")
-biocLite("EBImage")
-library("EBImage")
-library(grDevices)
-img_dir <- "../data/training_set/images/"
-n_files <- length(list.files(img_dir))
-
--- lib/feature.R
+++ lib/feature.R
@@ -2,7 +2,7 @@
 ### Construct visual features for training/testing images ###
 #############################################################
 
-### Authors: Yuting Ma/Tian Zheng
+### Authors: Leo Lam / Peifeng Hong
 ### Project 3
 ### ADS Spring 2017
 
@@ -13,27 +13,47 @@ feature <- function(img_dir, set_name, data_name="data", export=T){
   
   ### Input: a directory that contains images ready for processing
   ### Output: an .RData file contains processed features for the images
+
   
-  ### load libraries
-  library("EBImage")
-  
-  n_files <- length(list.files(img_dir))
-  
-  ### determine img dimensions
-  img0 <-  readImage(paste0(img_dir, "img", "_", data_name, "_", set_name, "_", 1, ".jpg"))
-  mat1 <- as.matrix(img0)
-  n_r <- nrow(img0)
-  
-  ### store vectorized pixel values of images
-  dat <- matrix(NA, n_files, n_r) 
-  for(i in 1:n_files){
-    img <- readImage(paste0(img_dir,  "img", "_", data_name, "_", set_name, "_", i, ".jpg"))
-    dat[i,] <- rowMeans(img)
+  ### RGB - required libraries
+  if(!require(EBImage)){
+    source('http://bioconductor.org/biocLite.R')
+    biocLite('EBImage')
+    library('EBImage')
   }
+  #information of image data
+  dir_names <- list.files(img_dir)
+  num_files <- length(list.files(img_dir))
+  
+  ### RGB - model
+  freq_RGB<- list()
+  ima<- list()
+  R <- 6; G <- 10; B <- 10
+  bin_r<- seq(0, 1, length.out=R); bin_g<- seq(0, 1, length.out=G); bin_b<- seq(0, 1, length.out=B)
+
+  
+  ft<- matrix(nrow=num_files, ncol = R * G * B)
+  
+  for(i in 1:num_files){
+    ima<- imageData(readImage(paste0(img_dir, dir_names[i])))
+    ima_RGB <- array(c(ima, ima, ima), dim =c(nrow(ima), ncol(ima), 3))
+    freq_RGB[[i]]<- as.data.frame(table(factor(findInterval(ima_RGB[, , 1], bin_r), levels=1:R), 
+                                        factor(findInterval(ima_RGB[, , 2], bin_g), levels=1:G), 
+                                        factor(findInterval(ima_RGB[, , 3], bin_b), levels=1:B)))
+    ft[i, ] <- as.numeric(freq_RGB[[i]]$Freq) / (ncol(ima) * nrow(ima))
+  }
+  
+  ft <- data.frame(ft)
+  
+  label_train<- read.csv("../data/training_set/label_train.csv")
+  names(label_train)<-c('ind','img','y')
+  ft$y<- label_train[,3]
+  
+  dat <- ft
   
   ### output constructed features
   if(export){
-    save(dat, file=paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+    saveRDS(dat, file=paste0("../output/feature_", data_name, "_", set_name, ".RData"))
   }
   return(dat)
 }
